37bc1551d81cadd4305a5a7f5651dca13753fca9
Philippe Tiede <philippetiede@gmail.com>
2021-05-31 13:03:10 +0200

    blalblalba

---
+++ week-7-security/iamroles-aws.sh
@@ -0,0 +1,8 @@
+# Create an IAM user
+aws iam create-user --user-name Filipenko
+
+# Create an IAM group
+aws iam create-group --group-name Filipenko-Gruppe
+
+# Add user to group
+aws iam add-user-to-group --user-name Filipenko --group-name Filipenko-Gruppe
